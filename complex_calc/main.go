77f9993901758c982e6c22422d99e8a7d81adad1
unknown <trivikram1999@gmail.com>
2021-08-19 10:40:41 +0530

    edit1

--- complex_calc/main.go
+++ complex_calc/main.go
@@ -14,26 +14,28 @@ func main() {
 	fmt.Println("Enter The expression")
 	consoleReader := bufio.NewReader(os.Stdin)
 	exp, _ := consoleReader.ReadString('\n')
+	
 
-	var ans float32
-	// fmt.Scanln(&exp)
+	var ans float64
+	// fmt.Scan(&exp)
+	// fmt.Println(exp)
 
-	res := strings.Fields(exp)
-
-	ops := []float32{}
+	res := strings.Fields(exp)// 5 + 9 -9..... res =[ 5 +  9 - 9]
+	// fmt.Println(res)
+	ops := []float64{}
 	opt := []string{}
 
 	for _, v := range res {
 		s, ok := strconv.Atoi(v)
 		if ok == nil {
-			ops = append(ops, float32(s))
+			ops = append(ops, float64(s)) //ops=[5 9 9]
 		} else {
-			opt = append(opt, v)
+			opt = append(opt, v) //opt= [+ -]
 		}
 
 	}
 	for _, v := range opt {
-		op1 := ops[0]
+		op1 := ops[0] 
 		op2 := ops[1]
 
 		ans = o.PerformOperation(op1, op2, v)
@@ -41,7 +43,7 @@ func main() {
 		if ops[2:] == nil {
 			break
 		}
-		ops = append([]float32{ans}, ops[2:]...)
+		ops = append([]float64{ans}, ops[2:]...)
 	}
-	fmt.Println("Result is", ans)
+	fmt.Printf("Result is %v", ans)
 }
--- complex_calc/operations/operator.go
+++ complex_calc/operations/operator.go
@@ -1,8 +1,9 @@
 package operations
+import("math")
 
 //PerformOperation is a function that performs operations
-func PerformOperation(op1 float32, op2 float32, opt string) float32 {
-	var ans float32
+func PerformOperation(op1 float64, op2 float64, opt string) float64 {
+	var ans float64
 
 	switch opt {
 	case "+":
@@ -15,7 +16,9 @@ func PerformOperation(op1 float32, op2 float32, opt string) float32 {
 		ans = op1 * op2
 	case "/":
 		if op2 == 0 {
+			return(math.Inf(1))
 			panic("Can not divide by Zero")
+			
 		}
 		ans = op1 / op2
 
