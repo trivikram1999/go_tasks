613f83f1f6b6bd12705f8555db2f6ebe28c75300
unknown <trivikram1999@gmail.com>
2021-08-20 17:11:04 +0530

    final

--- complex_calc/operations/operator.go
+++ complex_calc/operations/operator.go
@@ -17,9 +17,8 @@ func PerformOperation(op1 float64, op2 float64, opt string) (float64, error) {
 	case "/":
 		if op2 == 0 {
 			return math.Inf(+1), fmt.Errorf("division by zero cannot be done")
-		} else {
-			return op1 / op2, nil
 		}
+		return op1 / op2, nil
 	default:
 		return 0, fmt.Errorf("wrong operator used")
 	}
